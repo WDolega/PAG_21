9e5d9cc16fa2ce3b3b53f240ff91eb7a208722b5
WDolega <82936095+WDolega@users.noreply.github.com>
2021-04-21 17:49:23 +0200

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -7,16 +7,25 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Ball {
-    protected String material;
-    private String sport;
-    protected String brand;
-    protected int size; // e.g. 3,4,5 for football
-    private int weight;
-    protected double suggPrice;
-    protected double radius;
-    private Map<String, Integer> standardBall = new HashMap<>();
+    private String material; // material of ball e.g. leather
+    private String sport; // specify sport that uses this ball
+    private String brand; // brand of the ball
+    private int size; // e.g. 3,4,5 for football
+    private int weight; // weight of the ball given in grams
+    private double suggPrice; // suggested price of the ball in store given in PLN
+    private double radius; // radius of the ball given in centimetres
+    private final Map<String, Integer> standardBall = new HashMap<>();
 
-
+    /**
+     *
+     * @param material
+     * @param sport
+     * @param brand
+     * @param size
+     * @param weight
+     * @param suggPrice
+     * @param radius
+     */
     public Ball(String material, String sport, String brand, int size, int weight, double suggPrice, double radius) {
         this.material = material;
         this.sport = sport;
@@ -32,62 +41,130 @@ public class Ball {
         standardBall.put("American Football", 9);
     }
 
+    /**
+     *
+     * @param material
+     */
     public void setMaterial(String material) {
         this.material = material;
     }
 
+    /**
+     *
+     * @param sport
+     */
     public void setSport(String sport) {
         this.sport = sport;
     }
 
+    /**
+     *
+     * @param brand
+     */
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
+    /**
+     *
+     * @param size
+     */
     public void setSize(int size) {
         this.size = size;
     }
 
+    /**
+     *
+     * @param weight
+     */
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
+    /**
+     *
+     * @param suggPrice
+     */
     public void setSuggPrice(double suggPrice) {
         this.suggPrice = suggPrice;
     }
 
+    /**
+     *
+     * @param discount given in percent
+     */
+    public void setDiscount(double discount) { // value of discount is given in percent
+        this.suggPrice *= (1 - discount/100);
+    }
+
+    /**
+     *
+     * @param radius
+     */
     public void setRadius(double radius) {
         this.radius = radius;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMaterial() {
         return material;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getSport() {
         return sport;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getSuggPrice() {
         return suggPrice;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getRadius() {
         return radius;
     }
 
+    /**
+     *
+     * @return
+     */
     public String toString() {
         return "[" + getClass().getSimpleName() +
                 " material=" + getMaterial() +
@@ -100,22 +177,26 @@ public class Ball {
                 ']';
     }
 
-    public void getInfo(String sport, String material) {
+    public void getInfo() {
         System.out.println(this.getSport() + " uses " + getClass().getSimpleName().toLowerCase() + "s made of a "
                 + this.getMaterial().toLowerCase());
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isStandard(){
-        if (standardBall.containsKey(sport)){
-            return size == standardBall.get(sport);
+        if (standardBall.containsKey(getSport())){
+            return size == standardBall.get(getSport());
             }
         else
         return false;
     }
 
     public void checkStandard(){
-        if (standardBall.containsKey(sport)){
-            if (size == standardBall.get(sport)){
+        if (standardBall.containsKey(getSport())){
+            if (size == standardBall.get(getSport())){
                 System.out.println("The ball is standard");
             }
             else { System.out.println("The ball is non-standard");
@@ -124,14 +205,26 @@ public class Ball {
         else { System.out.println("No record of the sport"); }
     }
 
+    /**
+     *
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Ball)) return false;
         Ball ball = (Ball) o;
-        return getSize() == ball.getSize() && getWeight() == ball.getWeight() && Double.compare(ball.getSuggPrice(), getSuggPrice()) == 0 && Double.compare(ball.getRadius(), getRadius()) == 0 && Objects.equals(getMaterial(), ball.getMaterial()) && Objects.equals(getSport(), ball.getSport()) && Objects.equals(getBrand(), ball.getBrand());
+        return getSize() == ball.getSize() && getWeight() == ball.getWeight() &&
+                Double.compare(ball.getSuggPrice(), getSuggPrice()) == 0 &&
+                Double.compare(ball.getRadius(), getRadius()) == 0 && Objects.equals(getMaterial(), ball.getMaterial())
+                && Objects.equals(getSport(), ball.getSport()) && Objects.equals(getBrand(), ball.getBrand());
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getMaterial(), getSport(), getBrand(), getSize(), getWeight(), getSuggPrice(), getRadius());
--- Main.java
+++ Main.java
@@ -4,16 +4,27 @@ package com.example.ball;
 
 public class Main {
     public static void main(String[] args) {
-        Ball ball1 = new Ball("Plastic", "Football", "Adidas", 6, 290, 100, 23);
+        Ball ball1 = new Ball("Plastic", "Football", "Adidas", 6, 290, 100, 11);
         Ball ball2 = new Ball("Rubber", "Basketball", "Spalding", 7, 570, 150, 11.5);
+        Ball ball3 = new Ball("Plastic", "Volleyball", "Mikasa", 5, 270, 90, 10);
         System.out.println(ball1);
-        ball1.getInfo(ball1.getSport(),ball1.getMaterial());
+        ball1.getInfo();
         System.out.println(ball1.isStandard());
         ball1.checkStandard();
         ball1.setSize(5);
         System.out.println(ball1.isStandard());
         ball1.checkStandard();
+        ball1.setSport("Volleyball");
+        ball1.setBrand("Mikasa");
+        ball1.setWeight(270);
+        ball1.setDiscount(10); // value in percent
+        ball1.setRadius(10);
+        System.out.println(ball1);
+        System.out.println(ball3);
         System.out.println(ball1.equals(ball2));
-
+        System.out.println(ball1.equals(ball3));
+        ball2.setMaterial("Leather");
+        ball2.setSuggPrice(140);
+        System.out.println(ball2);
     }
 }
